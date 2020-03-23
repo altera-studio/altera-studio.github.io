37a758a3a47d0bffaf5e246d86929777cabba717
altera-studio <62520151+altera-studio@users.noreply.github.com>
2020-03-22 20:17:44 -0400

    Add files via upload

---
+++ assets/js/firebase/recommended-build.js
@@ -0,0 +1,32 @@
+// Add a callback (promise)
+function getBuildForChampion(name, callback)
+{
+	var name = name.toLowerCase();
+	var recommended = [];
+
+	$.getJSON('http://ddragon.leagueoflegends.com/cdn/10.4.1/data/en_US/item.json', function(results) {
+		var items = results.data;
+		var champions = firebase.database().ref('champions');
+
+		console.log(items);
+
+		champions.once('value', function(snapshot) {
+		    snapshot.forEach(function(childSnapshot) {
+		      	var obj = childSnapshot.val();   
+		      	var championName = obj.name.toLowerCase();
+
+		      	if(name == championName)
+		      	{
+		      		var build = obj.items.split(',');
+
+			      	for (var id in items)
+				    	for(var i in build) 
+				    		if(items[id].name == build[i]) 
+				    			recommended.push('http://ddragon.leagueoflegends.com/cdn/10.4.1/img/item/' + items[id].image.full); // retrieve image from items
+		      	} 
+		    });
+
+			return callback(recommended);
+		});
+	});
+}
