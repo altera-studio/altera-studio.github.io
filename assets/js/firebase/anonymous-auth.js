0aa95ad0331b34742510c71daaa424e985296c92
amakos <amakos404@users.noreply.github.com>
2020-03-27 10:01:39 -0400

    commit

--- assets/js/firebase/anonymous-auth.js
+++ assets/js/firebase/anonymous-auth.js
@@ -3,26 +3,6 @@ firebase.auth().signInAnonymously().then(function(user)
 	var isNewUser = user.additionalUserInfo.isNewUser
 	var uid = user.user.uid;
 
-	if(isNewUser)
-	{
-		// Bucket users into two groups, A and B, on first sign in only
-		var ABvalue = Math.random();
-
-		if(ABvalue < .5) {
-			console.log('User is added to test A group');
-
-			firebase.database().ref().child('users/' + uid).update({
-				group: 'A'
-			});
-		}
-		else {
-			console.log('User is added to test B group');
-			
-			firebase.database().ref().child('users/' + uid).update({
-				group: 'B'
-			});
-		}
-	}
 }).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
