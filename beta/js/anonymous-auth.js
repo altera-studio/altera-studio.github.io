b28d5732fdc0c87ab5b966e62f58954b815d119c
altera-studio <altera-studio@users.noreply.github.com>
2020-05-25 03:01:04 -0400

    commit

--- beta/js/anonymous-auth.js
+++ beta/js/anonymous-auth.js
@@ -1,6 +1,9 @@
-auth.signInAnonymously().catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
+function anonymousAuth() 
+{
+	auth.signInAnonymously().catch(function(error) {
+	  // Handle Errors here.
+	  var errorCode = error.code;
+	  var errorMessage = error.message;
+	  // ...
+	});
+}
